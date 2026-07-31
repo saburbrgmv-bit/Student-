1087f2379b0ee70701e6e89d2c3c08c3a1c6d2e1
Sabur Ibragimov <sizning_emailingiz@gmail.com>
2026-07-31 18:14:42 +0500

    push

--- student/serializers.py
+++ student/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Student
 
 class StudentSerializers(serializers.ModelSerializer):
-  model = Student
-  fields = ['full_name', 'age', 'bio', 'cours']
-  read_only_fields = ['create_at']
+  class Meta:
+   model = Student
+   fields = ['full_name', 'age', 'bio', 'cours']
+   read_only_fields = ['create_at']
